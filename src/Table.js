b6b37055c5f599ad6084b7c96810db3bdac829e6
Wandole <mchirez@gmail.com>
2022-10-11 15:13:14 +0200

    Rows are filled from datas given to Table component

--- src/Table.js
+++ src/Table.js
@@ -1,9 +1,29 @@
+// Il faut fournir un objet pour le nom des colonnes
+// Datas est un array d'objet. Chaque objet a un id unique.
+// Les champs des objets datas sont des strings
 function Table({ headers, datas }) {
+  const getKeys = () => {
+    return Object.keys(headers).filter((key) => key !== 'id')
+  }
+
   const headersDOM = () => {
     if (!headers) return
-    const keys = Object.keys(headers)
-    return keys.map((header) => {
-      return <th key={header}>{headers[header]}</th>
+    const keys = getKeys()
+    return keys.map((key) => {
+      return <th key={key}>{headers[key]}</th>
+    })
+  }
+
+  const rowsDOM = () => {
+    const keys = getKeys()
+    return datas.map((data) => {
+      return <tr key={data.id}>{getCells(keys, data)}</tr>
+    })
+  }
+
+  const getCells = (keys, data) => {
+    return keys.map((key) => {
+      return <td key={data.id + key}>{data[key]}</td>
     })
   }
 
@@ -25,19 +45,7 @@ function Table({ headers, datas }) {
               </td>
             </tr>
           </tfoot>
-          <tbody>
-            <tr>
-              <td>cell1_1</td>
-              <td>cell2_1</td>
-              <td>cell3_1</td>
-              <td>cell4_1</td>
-              <td>cell5_1</td>
-              <td>cell6_1</td>
-              <td>cell7_1</td>
-              <td>cell8_1</td>
-              <td>cell9_1</td>
-            </tr>
-          </tbody>
+          <tbody>{rowsDOM()}</tbody>
         </table>
       </div>
     </div>
