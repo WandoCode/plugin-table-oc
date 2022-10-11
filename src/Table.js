70e5d71310a3b9667fd9e8114e2ce428c863f310
Wandole <mchirez@gmail.com>
2022-10-11 12:35:05 +0200

    Project init

---
+++ src/Table.js
@@ -0,0 +1,46 @@
+function Table({ headers, datas }) {
+  const headersDOM = () => {
+    const keys = Object.keys(headers)
+    return keys.map((header) => {
+      return <th key={header}>{headers[header]}</th>
+    })
+  }
+
+  return (
+    <>
+      <div className="table">
+        <table className="blueTable">
+          {headers && (
+            <thead>
+              <tr>{headersDOM()}</tr>
+            </thead>
+          )}
+          <tfoot>
+            <tr>
+              <td colSpan="9">
+                <div className="links">
+                  {/* Todo: Footer: mettre pagination; btn next and prev;*/}
+                </div>
+              </td>
+            </tr>
+          </tfoot>
+          <tbody>
+            <tr>
+              <td>cell1_1</td>
+              <td>cell2_1</td>
+              <td>cell3_1</td>
+              <td>cell4_1</td>
+              <td>cell5_1</td>
+              <td>cell6_1</td>
+              <td>cell7_1</td>
+              <td>cell8_1</td>
+              <td>cell9_1</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  )
+}
+
+export default Table
